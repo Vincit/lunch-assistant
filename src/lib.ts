926b295e84102e858869b480b92ab4bf4c91aad3
Markus Palonen <markus.palonen@vincit.fi>
2023-10-07 17:51:44 +0300

    Add function to prompt OpenAI
    Querying OpenAI API requires the following environment variables to be set:
    - OPENAI_API_URL
    - OPENAI_API_KEY

--- src/lib.ts
+++ src/lib.ts
@@ -17,3 +17,33 @@ export const postToSlack = async (url: string, text: string): Promise<void> => {
     console.log(error.message);
   }
 };
+
+export const queryOpenAI = async (url: string, prompt: string): Promise<string> => {
+  try {
+    const payload = {
+      "messages": [{
+        "role": "user",
+        "content": prompt
+      }],
+      "temperature": 0.7,
+      "top_p": 0.95,
+      "frequency_penalty": 0,
+      "presence_penalty": 0,
+      "max_tokens": 800,
+      "stop": null
+    };
+    const headers = {
+      "Content-Type": "application/json",
+      "api-key": process.env.OPENAI_API_KEY,
+    };
+
+    console.log('Sending OpenAI request... POST request to: ' + url);
+    const response: AxiosResponse = await axios.post(url, payload, { headers });
+    console.log(response.status);
+    return response.data.choices[0].message.content;
+  } catch (error: any) {
+    console.log('Error in OpenAI integration!');
+    console.log(error.message);
+    return '';
+  }
+}
---
+++ src/openai.ts
@@ -0,0 +1,11 @@
+import { queryOpenAI } from "./lib";
+
+const url = process.env.OPENAI_API_URL || '';
+const prompt = process.argv[2] ?? 'Say this is a test';
+
+queryOpenAI(url, prompt).then((completion) => {
+    if (completion) {
+        console.log('OpenAI response:');
+        console.log(completion);
+    }
+});
