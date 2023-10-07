af141a144104fd2373fa44b0dae50294eeee2567
Riku Hytönen <riku.hytonen@vincit.fi>
2023-10-07 15:21:03 +0300

    change scraper to return the parsed list

--- scraper.js
+++ scraper.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 import cheerio from 'cheerio'
 import he from 'he'
 
-export const scrape = () => {
+export const scrape = async () => {
   const date = process.argv[2] ?? '2023-10-02'
 
   const url = 'https://www.compass-group.fi/menuapi/week-menus?costCenter=3024&date='+date+'T09%3A18%3A46.441Z&language=fi';
-  fetch(url)
+  return fetch(url)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP Error! Status: ${response.status}`)
@@ -15,7 +15,7 @@ export const scrape = () => {
   })
   .then((data) => {
     if (data.menus) {
-        data.menus.map((day, idx) => {
+        return data.menus.map((day, idx) => {
           if (idx < 5) {
             const parts = day.html.split('\n')
 
@@ -28,8 +28,9 @@ export const scrape = () => {
                 return decodedText
               })
 
-            console.log(cleanedParts)
+            return cleanedParts
           }
+          return ['UIINA']
         })
     }
   })
