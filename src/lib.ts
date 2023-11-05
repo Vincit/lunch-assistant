b916bd1ccf4a37e763dce055442060dddc45429f
Tatu Päkkilä <tatu.pakkila@vincit.fi>
2023-11-05 13:10:27 +0200

    Code cleanup, no need to duplcate rawdata.

--- src/lib.ts
+++ src/lib.ts
@@ -35,7 +35,7 @@ export const postToSlack = async (slackUrl: string, headline: string, message: s
   }
 };
 
-export const queryOpenAI = async (url: string, prompt: string, rawData: string): Promise<string> => {
+export const queryOpenAI = async (url: string, prompt: string): Promise<string> => {
   try {
     const payload = {
       "messages": [{
@@ -61,7 +61,7 @@ export const queryOpenAI = async (url: string, prompt: string, rawData: string):
   } catch (error: any) {
     console.log('Error in OpenAI integration!');
     console.log(error.message);
-    return 'Valitettavasti Azure OpenAI ei vastaa, mutta tässä lounaslista sellaisenaan: ' + rawData;
+    return 'Valitettavasti Azure OpenAI ei vastaa, mutta tässä lounaslista sellaisenaan.';
   }
 }
 
--- src/openai.ts
+++ src/openai.ts
@@ -3,7 +3,7 @@ import { queryOpenAI } from "./lib";
 const url = process.env.OPENAI_API_URL || '';
 const prompt = process.argv[2] ?? 'Say this is a test';
 
-queryOpenAI(url, prompt, 'Test').then((completion) => {
+queryOpenAI(url, prompt).then((completion) => {
     if (completion) {
         console.log('OpenAI response:');
         console.log(completion);
